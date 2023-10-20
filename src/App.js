e1421b52076bc88d1fe1488478ba073dfd8a675f
sar653 <sarva653@gmail.com>
2023-10-20 14:42:59 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addHandler = () => {
+    if (todo !== "") {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos]);
+      setTodo(""); // Clear the input field
+    }
+  };
+
+  const changeHandler = (e) => {
+    setTodo(e.target.value);
+  };
+   
+  const deleteHandler=(id)=>{
+    const updatedTodo=todos.filter((tod)=>tod.id!==id);
+    setTodos(updatedTodo)
+
+
+  }
+ const  editHandler=(id)=>{
+
+ }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Enter todo</label>
+      <input onChange={changeHandler} type="text" value={todo} />
+      <button onClick={addHandler}>Add todo</button>
+      <div>
+        <h1>
+          <ul>
+            {todos.map((a) => (
+              <li key={a.id}>{a.todo}<button onClick={()=>deleteHandler(a.id)}></button>
+             < button onClick={()=>editHandler(a.id)}></button></li>
+            ))}
+          </ul>
+        </h1>
+      </div>
     </div>
   );
 }
 
 export default App;
+
